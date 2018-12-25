013863e308af0b4ce7a34d855c4f6ab2314db2ec
DL <dvdgege5@gmail.com>
2018-12-24 18:04:02 -0800

    define drawLogo() function

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 /**
- * The main function. Here, we get the canvas' context and start drawing
+ * The main function. Here, we get the canvas' context and start drawing.  
+ * This function also contains the default values
  */
 function main() {
-	var canvas = document.getElementById("screenCanvas");
-	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0, 0, 150, 75);
+	drawLogo ( 1, 1 );
+
+
+
 }
 
+
+
+
+/**
+ * Draw the logo (a rectangle/square) on the canvas
+ * @param {*} x top-left x coordinate
+ * @param {*} y top-left y coordinate
+ * @param {*} width width
+ * @param {*} height height
+ * @param {*} fill color of the rectangle
+ */
+function drawLogo ( x, y, width=64, height=64, fill="#FF0000" ) {
+	// get the canvas and context
+	var canvas = document.getElementById("screenCanvas");
+	var ctx = canvas.getContext("2d");
+
+	// set the fill color
+	ctx.fillStyle = fill;
+
+	// draw the rectangle/square
+	ctx.fillRect ( x, y, x+width, y+height );
+}
+
+
+
+// invoke main function
 main();
